0be0fc4afbb651ffcc162e21e3848b41f110a1cb
sharathcv <sharathcv@hotmail.com>
2025-04-01 00:59:42 +1000

    Added MoviesGrid component base version added

---
+++ moviedux/src/components/MoviesGrid.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import '../styles.css';
+
+// We can also use a name instead of default
+export default function MoviesGrid() {
+    const [movies, setMovies] = useState([]);
+    
+    return (
+        <div></div>
+    );
+}
