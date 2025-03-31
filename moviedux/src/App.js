5b55b72b820c2bc394cdcf70e75f50dd2976766a
sharathcv <sharathcv@hotmail.com>
2025-03-31 13:05:27 +1000

    First check-in with basic app created, static resources and custom css added

---
+++ moviedux/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Welcome to Moviedux</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
