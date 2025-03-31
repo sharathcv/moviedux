e5254dbb1f1f8705d2fd13b1d422ca47e1ba18ac
sharathcv <sharathcv@hotmail.com>
2025-03-31 17:57:50 +1000

    First component created and added - Header component

--- moviedux/src/App.js
+++ moviedux/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>Welcome to Moviedux</h1>
-      </header>
+      <div className='container'>
+        <Header></Header>
+
+      </div>
     </div>
   );
 }
