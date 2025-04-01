e6cd37a839c5a018e1bf5e7342623fceb7b17d41
sharathcv <sharathcv@gmail.com>
2025-04-01 11:53:42 +1000

    Basic error handler for image tag

--- moviedux/src/components/MovieCard.js
+++ moviedux/src/components/MovieCard.js
@@ -3,9 +3,14 @@ import '../styles.css';
 
 // We can also use a name instead of default
 export default function MovieCard({movie}) {
+
+    const handleError = (e) => {
+        e.target.src = "images/default.jpg";
+    }
+
     return (
         <div key={movie.id} className="movie-card">
-            <img src={`images/${movie.image}`} alt={movie.title}/>
+            <img src={`images/${movie.image}`} alt={movie.title} onError={handleError}/>
             <div className='movie-card-info'>
                 <h3 className='movie-card-title'>{movie.title}</h3>
                 <h3 className='movie-card-genre'>{movie.genre}</h3>
