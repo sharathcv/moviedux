7395c2364c96da7abb36a2a75d4dd41357bfb7c5
sharathcv <sharathcv@gmail.com>
2025-04-01 15:27:07 +1000

    Filter logic on grid added

--- moviedux/src/components/MovieCard.js
+++ moviedux/src/components/MovieCard.js
@@ -8,13 +8,23 @@ export default function MovieCard({movie}) {
         e.target.src = "images/default.jpg";
     }
 
+    const getRatingClass = (rating) => {
+        if (rating >= 8) {
+            return 'rating-good';
+        } if (rating >=5 && rating < 8) {
+            return 'rating-ok';
+        }
+
+        return 'rating-bad';
+    }
+
     return (
         <div key={movie.id} className="movie-card">
             <img src={`images/${movie.image}`} alt={movie.title} onError={handleError}/>
             <div className='movie-card-info'>
                 <h3 className='movie-card-title'>{movie.title}</h3>
                 <h3 className='movie-card-genre'>{movie.genre}</h3>
-                <h3 className='movie-card-rating'>{movie.rating}</h3>
+                <h3 className={`movie-card-rating ${getRatingClass(movie.rating)}`}>{movie.rating}</h3>
             </div>
         </div>
     );
--- moviedux/src/components/MoviesGrid.js
+++ moviedux/src/components/MoviesGrid.js
@@ -5,6 +5,10 @@ import MovieCard from './MovieCard';
 // We can also use a name instead of default
 export default function MoviesGrid() {
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [genre, setGenre] = useState("All Genres");
+    const [rating, setRating] = useState("All");
+
     useEffect((() => {
 
         fetch("movies.json")
@@ -12,13 +16,50 @@ export default function MoviesGrid() {
             .then(data => setMovies(data));
 
     }), []); // empty array defines the condition on which useEffect will be called.
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+    const filteredMovies = movies.filter(movie =>
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <div className='movies-grid'>
-            {
-                movies.map(movie => (
-                    <MovieCard movie={movie} key={movie.id}></MovieCard>
-                ))
-            }
+        <div>
+            <input type="text" 
+                className="search-input" 
+                value={searchTerm}
+                onChange={handleSearchChange}
+                placeholder="Search movies..."/>
+            <div className="filter-bar">
+                <div className="filter-slot">
+                    <label>Genre</label>
+                    <select className="filter-dropdown">
+                        <option>All Genres</option>
+                        <option>Action</option>
+                        <option>Drama</option>
+                        <option>Fantasy</option>
+                        <option>Horror</option>
+                    </select>
+                </div>
+                <div className="filter-slot">
+                    <label>Rating</label>
+                    <select className="filter-dropdown">
+                        <option>All</option>
+                        <option>Good</option>
+                        <option>Ok</option>
+                        <option>Bad</option>
+                    </select>                    
+                </div>
+            </div>
+            <div className='movies-grid'>
+                {
+                    filteredMovies.map(movie => (
+                        <MovieCard movie={movie} key={movie.id}></MovieCard>
+                    ))
+                }
+            </div>
         </div>
     );
 }
